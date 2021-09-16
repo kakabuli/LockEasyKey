5c2968f19a46d7ba5314c1943097d1037ea34ac5
liuhaifeng <liuhaifeng@kaadas.com>
2021-09-16 14:53:28 +0800

    [Updated]替换正式线的隐私政策和用户协议

--- app/src/main/java/com/philips/easykey/lock/utils/ConstantConfig.java
+++ app/src/main/java/com/philips/easykey/lock/utils/ConstantConfig.java
@@ -10,5 +10,10 @@ public class ConstantConfig {
     public static final String MQTT_BASE_URL = BuildConfig.MQTT_HOST;//正式服务器
     public static final String LINPHONE_URL = BuildConfig.SIP_HOST;//正式sip
 
-
+    public static final String PHILIPS_PRIVACY_POLICY = "https://app.cone-x.com/mobile_h5/Philips_Privacy_Policy";
+    public static final String PHILIPS_TERMS_OF_USE = "https://app.cone-x.com/mobile_h5/Philips_Terms_of_Use";
+    public static final String PHILPS_DURRESS_HELP = "https://app.cone-x.com/mobile_h5/Philps_Durress_Help";
+    public static final String PHILIPS_UPDATE_ADMIN_PASSWORD = "https://app.cone-x.com/mobile_h5/Philips_Update_Admin_Password";
+    public static final String PHILIPS_ADD_SCAN_DEVICE = "https://app.cone-x.com/mobile_h5/Philips_Add_Scan_Device";
+    public static final String PHILIPS_FQA = "https://app.cone-x.com/mobile_h5/Philips_FQA";
 }
